2e48572c20b1cac22363a62267eb85d0d38b58ce
CarEsteban <car23016@uvg.edu.gt>
2024-05-28 01:03:56 -0600

    inicialización del proyecto con spring boot

---
+++ src/test/java/com/proyecto2/AppRecomendaciones/AppRecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto2.AppRecomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRecomendacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
